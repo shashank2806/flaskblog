c61c57fa7c3b3f630899a1078eb783824e0bb0bf
Shashank Shekhar <shashank.shekhar28june@gmail.com>
2018-06-30 21:05:48 +0530

    Use Jinja 2 for templating

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Shashank Shekhar',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 30, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 30, 2018'
+    }
+]
+
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
